d7979fc0bf81c5b6c39fbcf1018d1781cdab16c0
Don-Alvin <alvindon41@gmail.com>
2022-10-22 10:11:41 +0300

    Hero section

--- Gedo-Holdings/src/components/Hero/Hero.jsx
+++ Gedo-Holdings/src/components/Hero/Hero.jsx
@@ -3,7 +3,27 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='home' id='home'>
+      <div className='home__container container'>
+        <div className='home__data'>
+          <h1 className='home__title'>Building Relationships</h1>
+          <p className='home__description'>
+            Putting relationships first with intergrity passion and commitment<br></br>
+            <span className='home__description-span'>Creating lifetime memories by rethinking construction</span>
+          </p>
+          <div className='projects-services__home-link'>
+            <a href='#works' className='works'>
+              <h3>Works</h3>
+            </a>
+            <a href='#services' className='services'>
+              <h3>Services</h3>
+            </a>
+          </div>
+          <a href='#' className='button home__button'>Subscribe</a>
+          <p className='subscribe__description'>Get the latest construction news and trends in the market</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
